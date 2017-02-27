757fea43df9d90afe2509ecfe201869e367372dc
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-02-27 14:14:45 -0800

    one keep going

--- lib/restaurant_bill.rb
+++ lib/restaurant_bill.rb
@@ -1,12 +1,26 @@
-# class RestaurantBill
-#   attr_reader :ordered_items
+class RestaurantBill
+  attr_reader :ordered_items
 
-#   def initialize
-#     @ordered_items = []
-#   end
+  def initialize
+    @ordered_items = []
+  end
 
-#   def order_item(item, cost)
-#     @ordered_items << [item, cost]
-#   end
+  def order_item(item, cost)
+    @ordered_items << [item, cost]
+  end
 
-# end
+  def total
+    @total = 0
+    @ordered_items.each do |line|
+      @total += line[1]
+    end
+    @tax = @total * (0.098)
+    @total_tax = @total + @tax
+  end
+
+  # def tipping(amount)
+  #   @total_tip = @total_tax + amount
+  #   return @total_tip
+  # end
+
+end
--- specs/restaurant_bill_spec.rb
+++ specs/restaurant_bill_spec.rb
@@ -2,3 +2,40 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/restaurant_bill'
+
+describe "RestaurantBill class" do
+
+  before do
+    @bill = RestaurantBill.new
+  end
+
+  it "Creates new instance of RestaurantBill class" do
+    @bill.class.must_equal RestaurantBill
+  end
+
+  it "Initializes with ordered_items" do
+    @bill.must_respond_to :ordered_items
+  end
+
+  it "Expects new items to be included in ordered_items" do
+    @bill.order_item("potatoes", 3).must_include ["potatoes", 3]
+  end
+
+  it "Has 3 elements when 3 elements are added to order_item" do
+    3.times { @bill.order_item("item", "price") }
+    @bill.ordered_items.length.must_equal 3
+  end
+
+  ##KEEP GOING
+
+  it "Returns total and combines with tax" do
+    3.times { @bill.order_item("item", 5) }
+    @bill.total.must_equal 16.47
+  end
+
+  # it "Can tip" do
+  #   tip_bill = @bill.order_item("potatoes", 5)
+  #   tip_bill.total.tipping(2).must_equal 7.49
+  # end
+
+end
